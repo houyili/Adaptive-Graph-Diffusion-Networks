feb8d8245e8b6ed5a9a4b8a1f88d7a7c5af73441
lihouyi <lihouyi@kuaishou.com>
2022-11-12 18:53:18 +0800

    print

---
+++ ogbn-proteins/src/agdn_tanh_and_mean.sh
@@ -0,0 +1,22 @@
+cd "$(dirname $0)"
+python -u ./main.py \
+    --model agdn \
+    --sample-type random_cluster \
+    --train-partition-num 6 \
+    --eval-partition-num 2 \
+    --eval-times 1 \
+    --lr 0.01 \
+    --advanced-optimizer \
+    --n-epochs 1500 \
+    --n-heads 6 \
+    --n-layers 6 \
+    --weight-style HC \
+    --dropout 0.4 \
+    --n-hidden 150 \
+    --input-drop 0.1 \
+    --attn-drop 0. \
+    --hop-attn-drop 0. \
+    --edge-drop 0.1 \
+    --norm none \
+    --K 2  --edge-agg-mode "none_softmax" --edge-att-act="tanh" --norm="avg"\
+    --gpu 3 --root "/home/lihouyi/ogb/datasets/" --log-file-name="tanh_no_softmax_mean"
