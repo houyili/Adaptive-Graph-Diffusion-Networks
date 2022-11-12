fe2edd5d06d3c286dd82f5111f8c41d77527ce59
lihouyi <lihouyi@kuaishou.com>
2022-11-12 19:54:39 +0800

    print

---
+++ ogbn-proteins/src/agdn_softplus_k1_head15.sh
@@ -0,0 +1,22 @@
+cd "$(dirname $0)"
+python -u ./main.py \
+    --model agdn \
+    --sample-type random_cluster \
+    --train-partition-num 6 \
+    --eval-partition-num 2 \
+    --eval-times 1 \
+    --lr 0.01 \
+    --advanced-optimizer \
+    --n-epochs 1500 \
+    --n-heads 15 \
+    --n-layers 6 \
+    --weight-style HC \
+    --dropout 0.4 \
+    --n-hidden 60 \
+    --input-drop 0.1 \
+    --attn-drop 0. \
+    --hop-attn-drop 0. \
+    --edge-drop 0.1 \
+    --norm none \
+    --K 1 --edge-agg-mode "none_softmax" --edge-att-act="softplus" --norm="avg"\
+    --gpu 2 --root "/home/lihouyi/ogb/gipa_plus/datasets/" --log-file-name="softplus_no_softmax_mean"
