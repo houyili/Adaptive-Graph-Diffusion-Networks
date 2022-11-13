54ca855c0625fe1b2e969439f19d8a5735c5c89b
lihouyi <lihouyi@kuaishou.com>
2022-11-13 17:18:22 +0800

    agdn_ma_softplus_k1_head200

--- ogbn-proteins/src/agdn_k_2_c20.sh
+++ ogbn-proteins/src/agdn_k_2_c20.sh
@@ -1,6 +1,6 @@
 cd "$(dirname $0)"
 python -u ./main.py \
-    --model agdn_ma \
+    --model agdn \
     --sample-type random_cluster \
     --train-partition-num 20 \
     --eval-partition-num 2 \
@@ -8,15 +8,15 @@ python -u ./main.py \
     --lr 0.01 \
     --advanced-optimizer \
     --n-epochs 1500 \
-    --n-heads 100 \
+    --n-heads 6 \
     --n-layers 6 \
     --weight-style HC \
     --dropout 0.4 \
-    --n-hidden 4 \
+    --n-hidden 150 \
     --input-drop 0.1 \
     --attn-drop 0. \
     --hop-attn-drop 0. \
     --edge-drop 0.1 \
     --norm none \
-    --K 1 --edge-agg-mode "none_softmax" --edge-att-act="softplus" --norm="avg"\
-    --gpu 0 --root "/home/lihouyi/ogb/dataset/" --log-file-name="agdn_ma_softplus_k1_head50"
+    --K 2 \
+    --gpu 2 --root "/home/lihouyi/ogb/datasets/" --log-file-name="origin"
