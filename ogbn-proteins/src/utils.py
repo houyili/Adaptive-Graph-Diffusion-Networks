7f5004dc50be3fa972493f814f8e885bc1362f1b
lihouyi <lihouyi@kuaishou.com>
2022-11-14 04:44:12 +0800

    add gip

--- ogbn-proteins/src/utils.py
+++ ogbn-proteins/src/utils.py
@@ -14,13 +14,13 @@ from gen_model import gen_model
 
 def seed(seed=0):
     random.seed(seed)
+    dgl.random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
-    dgl.random.seed(seed)
 
 def compute_norm(graph):
     degs = graph.in_degrees().float().clamp(min=1)
