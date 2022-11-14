48572b7df6b17aa573d5b08cd9de56dcc8fbc4f2
lihouyi <lihouyi@kuaishou.com>
2022-11-15 00:13:57 +0800

    debug

---
+++ ogbn-proteins/src/agdn_k1_cluster1000.sh
@@ -0,0 +1,24 @@
+
+cd "$(dirname $0)"
+python -u ./mainc.py \
+    --model agdn \
+    --sample-type m_cluster \
+    --train-partition-num 1000 \
+    --sampler-K 10\
+    --eval-partition-num 10 \
+    --eval-times 1 \
+    --lr 0.01 \
+    --advanced-optimizer \
+    --n-epochs 1500 \
+    --n-heads 6 \
+    --n-layers 6 \
+    --weight-style HC \
+    --dropout 0.4 \
+    --n-hidden 150 \
+    --input-drop 0.1 \
+    --attn-drop 0. \
+    --hop-attn-drop 0. \
+    --edge-drop 0.1 \
+    --norm none \
+    --K 1 \
+    --gpu 4 --root "/data/ogb/datasets/" --log-file-name="k1_m_cluster1k" --n-runs=5
