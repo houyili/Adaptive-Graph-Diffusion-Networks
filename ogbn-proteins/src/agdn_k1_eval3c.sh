55d92423576f25fdbe505d90cd8d2f939f47c5e6
lihouyi <lihouyi@kuaishou.com>
2022-11-14 19:51:40 +0800

    add mainc.py

---
+++ ogbn-proteins/src/agdn_k1_eval3c.sh
@@ -0,0 +1,22 @@
+cd "$(dirname $0)"
+python -u ./mainc.py \
+    --model agdn \
+    --sample-type random_cluster \
+    --train-partition-num 6 \
+    --eval-partition-num 3 \
+    --eval-times 1 \
+    --lr 0.01 \
+    --advanced-optimizer \
+    --n-epochs 1500 \
+    --n-heads 6 \
+    --n-layers 6 \
+    --weight-style HC \
+    --dropout 0.4 \
+    --n-hidden 150 \
+    --input-drop 0.1 \
+    --attn-drop 0. \
+    --hop-attn-drop 0. \
+    --edge-drop 0.1 \
+    --norm none \
+    --K 1 \
+    --gpu 2 --root "/data/ogb/datasets/" --log-file-name="k1_eval3c" --n-runs=5
