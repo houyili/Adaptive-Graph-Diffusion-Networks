abf87cbb3ce46deac79b51432aa16ac8b22c9d85
lihouyi <lihouyi@kuaishou.com>
2022-11-18 01:59:03 +0800

    in_seed_sample

---
+++ ogbn-proteins/src/sh_gat/gipa_in_seed_ful6.sh
@@ -0,0 +1,18 @@
+cd "$(dirname $0)"
+python -u ../main_gipa.py \
+          --n-runs 10 \
+          --gpu 5 \
+          --lr 0.01 \
+          --n-hidden 50 \
+          --batch_rate 6 \
+          --root "/data/ogb/datasets/"\
+          --cpu-start-from 75 \
+          --sample-no-limit \
+          --sample-type in_seed_sample_full\
+          --agg-batch-norm \
+          --edge-att-act softplus \
+          --n-heads 20 \
+          --eval-batch-rate 3 \
+          --edge-drop 0.0 \
+          --dropout 0.4 \
+          --n-layers 5
