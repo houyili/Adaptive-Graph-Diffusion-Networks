beba9b47a824a621bd342b46cadd7684afd769f3
lihouyi <lihouyi@kuaishou.com>
2022-11-20 00:08:21 +0800

    gipa_para_layer5.sh

---
+++ ogbn-proteins/src/a100_run/gipa_para_layer5.sh
@@ -0,0 +1,23 @@
+cd "$(dirname $0)"
+python -u ../main3.py \
+    --model gipa_para \
+    --sample-type random_cluster \
+    --train-partition-num 6 \
+    --eval-partition-num 1 \
+    --eval-times 1 \
+    --lr 0.01 \
+    --advanced-optimizer \
+    --n-epochs 1500 \
+    --n-heads 20 \
+    --n-layers 5 \
+    --dropout 0.4 \
+    --n-hidden 200 \
+    --input-drop 0.1 \
+    --norm none \
+    --K 1 \
+    --edge-agg-mode "none_softmax" \
+    --edge-att-act="softplus"\
+    --gpu 0 \
+    --root "/data/ogb/datasets/"\
+    --n-hidden-per-head 40 \
+    --log-file-name="gipa_para_layer5"
