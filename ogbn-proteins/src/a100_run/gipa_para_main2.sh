16e7bdc912422b813d9d74b0158de1db9cf15b4b
lihouyi <lihouyi@kuaishou.com>
2022-11-24 14:10:22 +0800

    add gipa_para_main2.sh

--- ogbn-proteins/src/a100_run/gipa_para_main2.sh
+++ ogbn-proteins/src/a100_run/gipa_para_main2.sh
@@ -11,7 +11,7 @@ python -u ../main2.py \
     --n-heads 20 \
     --n-layers 6 \
     --dropout 0.4 \
-    --n-hidden 80 \
+    --n-hidden 60 \
     --input-drop 0.1 \
     --norm none \
     --K 1 \
@@ -20,5 +20,5 @@ python -u ../main2.py \
     --gpu 0 \
     --root "/data/ogb/datasets/"\
     --n-runs 10 \
-    --n-hidden-per-head 45 \
+    --n-hidden-per-head 47 \
     --log-file-name="gipa_para_main2"
