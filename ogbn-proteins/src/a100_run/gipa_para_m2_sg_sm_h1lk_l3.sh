ba3194d8680086a770a8d61fcf7b711783e23677
lihouyi <lihouyi@kuaishou.com>
2022-11-25 13:41:44 +0800

    l1~l5

---
+++ ogbn-proteins/src/a100_run/gipa_para_m2_sg_sm_h1lk_l3.sh
@@ -0,0 +1,26 @@
+cd "$(dirname $0)"
+python -u ../main2.py \
+    --model gipa_para \
+    --sample-type random_cluster \
+    --train-partition-num 6 \
+    --eval-partition-num 2 \
+    --eval-times 1 \
+    --lr 0.01 \
+    --advanced-optimizer \
+    --n-epochs 1500 \
+    --n-heads 20 \
+    --n-layers 3 \
+    --dropout 0.4 \
+    --n-hidden 100 \
+    --input-drop 0.1 \
+    --attn-drop 0. \
+    --hop-attn-drop 0. \
+    --edge-drop 0.1 \
+    --edge-agg-mode "single_softmax" \
+    --edge-att-act "none" \
+    --norm none \
+    --gpu 6 \
+    --root "/data/ogb/datasets/"\
+    --n-runs 10 \
+    --n-hidden-per-head 50 \
+    --log-file-name="gipa_para_m2_sg_sm_h1lk_l3"
--- ogbn-proteins/src/utils.py
+++ ogbn-proteins/src/utils.py
@@ -65,3 +65,16 @@ def get_act_by_str(name:str, negative_slope:float=0):
     else:
         res = nn.Softplus()
     return res
+
+
+class FocalLoss(nn.modules.loss.BCEWithLogitsLoss):
+    def __init__(self, weight=None, gamma=2, reduction='mean'):
+        super(FocalLoss, self).__init__(weight,reduction=reduction)
+        self.gamma = gamma
+        self.weight = weight #weight parameter will act as the alpha parameter to balance class weights
+
+    def forward(self, input, target):
+        ce_loss = F.cross_entropy(input, target, reduction=self.reduction, weight=self.weight)
+        pt = torch.exp(-ce_loss)
+        focal_loss = ((1 - pt) ** self.gamma * ce_loss).mean()
+        return focal_loss
